24d1f98210ec166ae70a9fd07d445946b8513760
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-08-27 18:41:30 +0200

    Remove proxy header

--- public/UnrealVoxelSim/Voxel/Rendering/Api/Rendering.h
+++
@@ -1,8 +0,0 @@
-// TODO remove proxy-header
-
-#pragma once
-
-#include "UnrealVoxelSim/Voxel/Rendering/Api/Mesh.h"
-#include "UnrealVoxelSim/Voxel/Rendering/Api/Snapshot.h"
-#include "UnrealVoxelSim/Voxel/Rendering/Api/SurfaceId.h"
-#include "UnrealVoxelSim/Voxel/Rendering/Api/Vertex.h"
